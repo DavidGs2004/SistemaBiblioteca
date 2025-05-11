146d4cb7e0ae3e1f0ecfcab8a63e4a4f1fd54787
DavidGs2004 <dg102090@gmail.com>
2025-05-10 21:19:40 -0600

    Login_Prueba

---
+++ RegistroUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrousuario;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class RegistroUsuario {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner (System.in);
+        System.out.println("Bienvenido");
+       
+       System.out.println("Ingrese nombre completo: ");
+       var nombrecompleto = scanner.nextLine();
+       
+       
+    }
+}
